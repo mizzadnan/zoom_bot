f6745531c0be04d70fb5b243e0058451b8c2ee7e
mizzadnan <mirza.adnan2205@gmail.com>
2020-11-17 19:43:55 +0600

    cleaned code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # importing the required modules
 
-import pyautogui, subprocess
+import pyautogui
+import subprocess
 from datetime import datetime
 from time import sleep
 
 # zoom ids and passes for every day of the week
 
-saturday = {"9207344064":"4dak8E", "4078407985":"tu@rumc", "2943572024":"szk@rumc"} # SB TU SZK
-sunday = {"9443918403":"nc@rumc", "4096669370":"gcs@rumc", "5887996472":"mhb@rumc"} # NC GCS MHB
-monday = {"6199024532":"mni@rumc", "4078407985":"tu@rumc", "9713219788":"yk@rumc"} # MNI TU YK
-tuesday = {"3187225592":"ms@rumc", "6442635633":"fr@rumc", "8068605560":"mzi@rumc"} # MS FR MZI
-wednesday = {"6166730587":"rai@rumc", "9713219788":"yk@rumc", "2134182170":"frn@rumc"} # RAI YK FRN
-thursday = {"6494909022":"aa@rumc", "8597574150":"mrn@rumc", "3187225592":"ms@rumc"} # AA MRN MS
+saturday = {"9207344064": "4dak8E", "4078407985": "tu@rumc", "2943572024": "szk@rumc"}  # SB TU SZK
+sunday = {"9443918403": "nc@rumc", "4096669370": "gcs@rumc", "5887996472": "mhb@rumc"}  # NC GCS MHB
+monday = {"6199024532": "mni@rumc", "4078407985": "tu@rumc", "9713219788": "yk@rumc"}  # MNI TU YK
+tuesday = {"3187225592": "ms@rumc", "6442635633": "fr@rumc", "8068605560": "mzi@rumc"}  # MS FR MZI
+wednesday = {"6166730587": "rai@rumc", "9713219788": "yk@rumc", "2134182170": "frn@rumc"}  # RAI YK FRN
+thursday = {"6494909022": "aa@rumc", "8597574150": "mrn@rumc", "3187225592": "ms@rumc"}  # AA MRN MS
 
 # zoom ids
 
@@ -30,11 +31,3 @@ monday_pass = list(monday.values())
 tuesday_pass = list(tuesday.values())
 wednesday_pass = list(wednesday.values())
 thursday_pass = list(thursday.values())
-
-
-
-
-
-
-
-
